eafbb3fe139c1375e83c9add51ddcbc810223e94
Yuri Bottale <yuribottale@gmail.com>
2025-04-26 11:30:08 +0200

    feat: Add explicit basename to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Importiamo il nostro CSS globale
-import { BrowserRouter } from 'react-router-dom'; // Importa
+import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+
+// Definisci il basename basato sul nome del repository
+const repoName = 'mio-portfolio'; // Assicurati sia il nome corretto
+const basename = `/${repoName}`;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Avvolgi qui */}
+    {/* AGGIUNGI LA PROP basename QUI */}
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
+
